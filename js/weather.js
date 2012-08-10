268cc4f743d5147b075f2780f11782611599f537
stefanoprea <stefanoprea@P4322.(none)>
2012-08-10 11:15:07 +0300

    Weather app javascript functional

--- js/weather.js
+++ js/weather.js
@@ -1,39 +1,33 @@
-function loadXML(url)
-{
-	var xmldoc = document.implementation.createDocument("", "", null);
-	xmldoc.load(url);
-	return xmldoc;
-}
-
-function getXmlRoot(xmldoc)
-{
-	return xmldoc.documentElement;
-}
-
-var string = 'http://www.google.com/ig/api?weather=';//http://www.google.com/ig/api?weather= + CityName
-var xmldoc;
-var weatherNode;
-
-function readCurrentWeatherConditions()
-{
-	xmldoc = loadXML(string + 'Cluj');
-	var i, tempFaNode, tempFaData, conditionData, cityLocationNode;
-	var humidityData, windConditionData, cityLocationData, forecastInformationNode;
-	var chNode = xmldoc.childNodes;
-	forecastInformationNode = weatherNode.firstChild;
-	cityLocationNode = forecastInformationNode.firstChild;
-	cityLocationData = cityLocatioNode.nodeValue;
-	for(i=0; i < chNode.length; i++)
-		conditionData = chNode[0].nodeValue;
-		tempFaData = chNode[1].nodeValue;
-		tempCeData = chNode[2].nodeValue;
-		humidityData = chNode[3].nodeValue;
-		windConditionData = chNode[5].nodeValue;
-}
-function readTextBox()
-{
+function readCurrentWeatherConditions() { 
 	var text_box = document.getElementById('type_box');
-	text = text_box.value;
-	//allert(text);
+	var text = text_box.value;
+	var params = 'operation=weather&location='+text;
+	var url = 'http://myth.dyndns-server.com:8333?'+params;
+//	var url = 'http://www.google.com/ig/api?weather='+text;
+	var xmlhttp = false;
+	try {
+		// Opera 8.0+, Firefox, Safari
+		xmlhttp = new XMLHttpRequest();
+	} catch (e) {
+			try {
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				// Something went wrong
+				alert("Your browser broke!");
+				return false;
+			}
+	}
+	xmlhttp.open("GET", url, false);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var obj = jQuery.parseJSON(xmlhttp.responseText);
+			document.getElementById('tempFa').innerHTML = "&nbsp;&nbsp;City: " + obj.location + ";<br/>"+"&nbsp;&nbsp;Grade: " + obj.tempc + ";<br/>";		
+			document.getElementById('icon').setAttribute('src',obj.generalicon); 
+		}
+	}
+	xmlhttp.send();
+	return false;
 }
+	
+
 
